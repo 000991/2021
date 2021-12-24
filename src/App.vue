08ae201e2326b0a9a7d27495aa3a7f2cdd3586f2
hahaha <771037278@qq.com>
2021-12-24 22:15:17 +0800

    更新样式

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,62 @@
 <template>
   <div class="mainContainer">
-    <div class="holiday" @click="clickToggcleClass">
-      <div class="user">
-        <div class="user-photo animate__animated" :class="{'animate__flip':isShowAnimate}">
-          <img :src="userface" alt="">
+    <div class="holiday">
+      <div
+        class="user animate__animated animate__infinite"
+        :class="{ animate__pulse: isShowAnimate }"
+      >
+        <!-- animate__flip , animate__zoomInUp-->
+        <div
+          class="user-photo animate__animated"
+          :class="{
+            animate__flip: isShowAnimate
+          }"
+        >
+          <img :src="userface" alt="" />
         </div>
-        <span class="user-text animate__animated" :class="{'animate__zoomInDown':isShowAnimate}">{{ userName }}</span>
+        <span
+          class="user-text animate__animated"
+          :class="{ animate__zoomInDown: isShowAnimate }"
+          >{{ userName }}</span
+        >
       </div>
       <!-- <div class="zhufu animate__animated" :class="{'animate__zoomInUp':isShowAnimate}">{{ fullTitle }}</div> -->
-      <div class="zhufu animate__animated" :class="{'animate__zoomInDown':isShowAnimate}">{{ fullTitle }}</div>
+      <div
+        class="zhufu animate__animated"
+        :class="{ animate__zoomInDown: isShowAnimate }"
+      >
+        <div
+          class="animate__animated animate__infinite animate__delay-5s"
+          :class="{ animate__swing: isShowAnimate }"
+        >
+          {{ fullTitle }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent, reactive, computed, nextTick } from 'vue'
+import {
+  ref,
+  defineComponent,
+  reactive,
+  computed,
+  nextTick,
+  onMounted
+} from 'vue'
 import utils from './utils/utils'
 import 'animate.css'
 export default defineComponent({
   name: 'App',
-  setup () {
+  setup() {
     const userName = utils.getQueryParam('user') || 'HAHA' // 用户名
     const text = utils.getQueryParam('text') // 显示文字
     const userface = ref('')
     userface.value = require('./assets/1.jpg')
     // const bg1 = ref('')
     // bg1.value = require('./assets/33.jpg')
+    // 节日信息配置
     const holiday = reactive({
       holidayName: '', // 节日名称
       holidayGreetings: '快乐!', // 祝福语
@@ -46,12 +77,12 @@ export default defineComponent({
         background: ''
       }
     })
-    var dateYMD = getDateYMD()
+    var dateYMD = utils.getDateYMD()
     const md = `${dateYMD.m}-${dateYMD.n}`
     switch (md) {
       case '12-24':
         holiday.holidayName = '平安夜'
-        setStyleBackgroud(holiday.pingan.background)
+        setStyleBackgroud(holiday.shengdan.background)
         break
       case '12-25':
         holiday.holidayName = '圣诞'
@@ -69,37 +100,41 @@ export default defineComponent({
     }
     // 显示内容
     const fullTitle = computed(() => {
-      return (
-        (text || holiday.holidayName + holiday.holidayGreetings)
-      )
+      return text || holiday.holidayName + holiday.holidayGreetings
     })
-
-    document.title = userName + '，' + fullTitle.value
-
-    function getDateYMD () {
-      var date = new Date()
-      var y = date.getFullYear()
-      var m = date.getMonth() + 1
-      var n = date.getDate()
-      // return y + '-' + m + '-' + n
-      return {
-        y, m, n
-      }
+    const emotions = utils.emotions()
+    function setPageTitle() {
+      const a = utils.randomNum(0, 4)
+      const b = utils.randomNum(0, 4)
+      document.title = `${emotions[a]}${userName}，${fullTitle.value}${emotions[b]}`
+      setTimeout(() => {
+        setPageTitle()
+      }, 1000)
     }
+    setPageTitle()
+
     // 设置背景
-    function setStyleBackgroud (background: string) {
+    function setStyleBackgroud(background: string) {
       document.body.style.background = background
     }
 
     const isShowAnimate = ref(true)
     // 切换动画样式
-    async function clickToggcleClass () {
+    async function clickToggcleClass() {
       isShowAnimate.value = false
       await nextTick()
       setTimeout(() => {
         isShowAnimate.value = true
       }, 100)
     }
+
+    onMounted(() => {
+      // 禁用右键
+      // window.oncontextmenu = function (e: Event) {
+      //   e.preventDefault()
+      // }
+      document.body.addEventListener('click', clickToggcleClass, true)
+    })
     return {
       userface,
       userName,
@@ -114,7 +149,7 @@ export default defineComponent({
 
 <style lang="scss">
 // @import url('https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css');
-*{
+* {
   padding: 0;
   margin: 0;
 }
@@ -140,31 +175,36 @@ body {
   text-align: center;
   font-size: 46px;
 }
-.user{
+.user {
   display: flex;
   justify-content: center;
   justify-items: center;
   padding-bottom: 20px;
-  .user-photo{
+  .user-photo {
     width: 80px;
     height: 80px;
     overflow: hidden;
-    margin-right: 10px;
+    margin-right: 20px;
     border-radius: 50%;
-    img{
+    img {
       width: 100%;
     }
   }
-  .user-text{
+  .user-text {
     line-height: 80px;
     color: #351c75;
     // text-shadow: 0 4px 10px rgba(0, 0, 0, 0.7);
-    text-shadow: 0 -6px 4px #ff0000, /*上*/ 4px -4px 4px #46ff00, /*右上*/ 6px 0 4px #32003c, /*右*/ 4px 4px 4px #ffffff, /*右下*/ 0 6px 4px #ffd500, /*下*/ -4px 4px 4px #ee00ff, /*左下*/ -6px 0 4px #0021ff, /*左*/ -4px -4px 4px #000000;
+    text-shadow: 0 -6px 4px #ff0000, /*上*/ 4px -4px 4px #46ff00,
+      /*右上*/ 6px 0 4px #32003c, /*右*/ 4px 4px 4px #ffffff,
+      /*右下*/ 0 6px 4px #ffd500, /*下*/ -4px 4px 4px #ee00ff,
+      /*左下*/ -6px 0 4px #0021ff, /*左*/ -4px -4px 4px #000000;
   }
 }
-.zhufu{
-  animation-duration: 1.5s;
+.zhufu {
+  // animation-duration: 1.5s;
+  --animate-duration: 3s;
   color: #ee3e3e;
-  text-shadow: 0 0 4px #32003c
+  // text-shadow: 0 0 4px #32003c;
+  text-shadow: 0 0 4px #efebf0;
 }
 </style>
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,12 +1,66 @@
 const utils = {
-  getQueryParam (name: string) {
+  getQueryParam(name: string) {
     const query = window.location.search.substring(1)
     const vars = query.split('&')
     for (let i = 0; i < vars.length; i++) {
       const pair = vars[i].split('=')
-      if (pair[0] === name) { return pair[1] }
+      if (pair[0] === name) {
+        return pair[1]
+      }
     }
     return ''
+  },
+  getDateYMD() {
+    const date = new Date()
+    const y = date.getFullYear()
+    const m = date.getMonth() + 1
+    const n = date.getDate()
+    // return y + '-' + m + '-' + n
+    return {
+      y,
+      m,
+      n
+    }
+  },
+  // 生成从minNum到maxNum的随机数
+  randomNum(minNum: number, maxNum: number): number {
+    switch (arguments.length) {
+      case 1:
+        return parseInt(String(Math.random() * minNum + 1), 10)
+        break
+      case 2:
+        return parseInt(
+          String(Math.random() * (maxNum - minNum + 1) + minNum),
+          10
+        )
+        break
+      default:
+        return 0
+        break
+    }
+  },
+  emotions() {
+    const emotionArr = [
+      '🍇',
+      '🍉',
+      '🍊',
+      '🍎',
+      '🍏',
+      '🍓',
+      '🍅',
+      '🍗',
+      '🍿',
+      '🍟',
+      '🍔',
+      '🥖',
+      '🧈',
+      '🍢',
+      '🍚',
+      '🍩',
+      '🍭',
+      '🎂'
+    ]
+    return emotionArr
   }
 }
 
